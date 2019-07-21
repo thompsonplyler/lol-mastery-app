a7254a4b04e0627940a2a3c5e96ccdd01b34ef20
Thompson Plyler <thompson@thompsonplyler.com>
2019-07-21 14:56:38 -0400

    Attaching badges to chop champs next.

--- src/containers/ChampionInfo.js
+++ src/containers/ChampionInfo.js
@@ -4,6 +4,16 @@ import '../ChampionInfo.css';
 import { Component } from 'react';
 import C from '../data/champion_constants'
 import * as bigData from '../data/champion.json'
+import m1 from '../assets/images/1.png'
+import m2 from '../assets/images/2.png'
+import m3 from '../assets/images/3.png'
+import m4 from '../assets/images/4.png'
+import m5 from '../assets/images/5.png'
+import m6 from '../assets/images/6.png'
+import m7 from '../assets/images/7.png'
+
+
+
 // import {
 //   BrowserRouter as Router,
 //   Route
@@ -15,20 +25,27 @@ import * as bigData from '../data/champion.json'
 class ChampInfo extends Component {
 
     champInfo = (data) => {
-        return data.map(champion=>{
+        console.log(data.length)
+        return data.map((champion, index)=>{
             
             let {championId, championLevel, championPoints} = champion
             let champName = C[championId]
-
-            console.log(champName)
-            console.log(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${championId}.png`)
-            return <div className="Champion-Info-Box">
-            {champName}<br />
-            <img src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${championId}.png`} /><br/>
+            if (index < 3) {return <div className="Champion-Info-Box-Top">
+            <h4>{champName}</h4>
+            <img className='champ-icon-mastery' src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${championId}.png`} /><br/>
+            {/* <img className='champ-icon-mastery-top' src="/images/1.png" /> */}
             Mastery Level: {championLevel}<br/>
             Mastery Points: {championPoints}
+        </div>}
+        else {
+            return <div className="Champion-Info-Box">
+             <h4>{champName}</h4>
+             <img className='champ-icon-mastery' src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${championId}.png`} /><br/>
+             Mastery Level: {championLevel}<br/>
+             Mastery Points: {championPoints}
+             </div>
+             }
 
-            </div>
         })
     }
 
